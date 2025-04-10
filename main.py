98bd3ac0a7c4b680c0b1c36a1a548f959e1acc7b
NickSheva <nikshevtsov@mail.ru>
2025-04-10 15:40:14 +0500

    add prettytable

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from lxml import html
 import time
 import requests
 from urllib.parse import urljoin
+from prettytable import PrettyTable
 
 BASE_URL = "https://lombard-perspectiva.ru"
 session = requests.Session()
@@ -14,8 +15,13 @@ def main():
     # Получаем сразу все ссылки на товары
     product_links = tree.xpath('//a[contains(@class, "product-list-item")]/@href')
     full_links = [urljoin(BASE_URL, link) for link in product_links]
+    table = PrettyTable()
+    table.field_names = ["№", "Links"]
+    table.align["№"] = "l"
+    for i, link in enumerate(full_links, 1):
+        table.add_row([i, link])
     print("--- %s seconds ---" % (time.time() - start_time))
-    return full_links
+    return table
     
 
 if __name__ == "__main__":
